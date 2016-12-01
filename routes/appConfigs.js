d6713da562c90c8ed3c2efeb7c8e01d7d6384d8b
furio <francesco@furio.me>
2016-12-01 10:29:48 +0100

    Multiple output formats

--- routes/appConfigs.js
+++ routes/appConfigs.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require("path");
 var repositoryLibrary = require(path.resolve(__dirname, "./../lib/repo"));
+var outConverterLibrary = require(path.resolve(__dirname, "./../lib/output-converter"));
 
 var generateRouter = function(configData) {
     var router = express.Router();
@@ -24,7 +25,9 @@ var generateRouter = function(configData) {
     router.get('/:application/:enviroment', function(req,res,next) {
         repository.getAppConfig(req.params.application, req.params.enviroment)
             .then(function(configData) {
-                return res.status(200).json(configData);
+                var outType = req.query.type || "json";
+                res.contentType("text/" + outType);
+                return res.status(200).send(outConverterLibrary(configData,outType));
             })
             .catch(function(err) {
                 return next(err);
