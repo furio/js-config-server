d48a35d6c78656d2015c5c925d7fb7bc9d20c26d
furio <francesco@furio.me>
2016-12-14 17:26:19 +0100

    Better code for recursive json reader

--- test/lib/repo-data-filters/json-encrypted-fields/index_test.js
+++ test/lib/repo-data-filters/json-encrypted-fields/index_test.js
@@ -3,9 +3,21 @@ var JsonEncryptedFieldsFilter = require('../../../../lib/repo-data-filters/json-
 
 describe('JsonEncryptedFieldsFilter({})', function() {
     describe('#filterData()', function() {
+        it('should return the empty JSON as input', function() {
+            var Filter = new JsonEncryptedFieldsFilter({});
+            var jsonData = {};
+            assert.deepEqual(jsonData, Filter.filterData(jsonData));
+        });
+
+        it('should return the same flat JSON as input', function() {
+            var Filter = new JsonEncryptedFieldsFilter({});
+            var jsonData = {a: "b", c: 2};
+            assert.deepEqual(jsonData, Filter.filterData(jsonData));
+        });
+
         it('should return the same JSON as input', function() {
             var Filter = new JsonEncryptedFieldsFilter({});
-            var jsonData = {data:"string", object:{data:"string", arr:[{a:{b:{c:1}}}, 2, "data"]}};
+            var jsonData = {data:"string", object:{data:"string", arr:[{a:{b:{c:1}}}, 2, "data", {}]}};
             assert.deepEqual(jsonData, Filter.filterData(jsonData));
         });
     });
