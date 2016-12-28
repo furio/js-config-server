e47e943b5c1523560c305db37c5152aa2f3340d1
furio <francesco@furio.me>
2016-12-28 01:24:12 +0100

    Refactor to repo-single

--- routes/appConfigs.js
+++ routes/appConfigs.js
@@ -5,7 +5,7 @@ var outConverterLibrary = require(path.resolve(__dirname, "./../lib/output-conve
 
 var generateRouter = function(configData) {
     var router = express.Router();
-    var repository = new Repository(configData);
+    var repository = new Repository(configData.repo);
 
     router.use(function(req,res,next) {
         repository.healthCheck()
